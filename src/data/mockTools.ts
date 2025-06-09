0cd5fc23aebab41772c58ffa8307888bd65ecf24
Nihith Penumuchu <135858540+NIHITHPENUMUCHU@users.noreply.github.com>
2025-06-09 11:11:30 +0530

    Updated mockTools.ts

--- src/data/mockTools.ts
+++ src/data/mockTools.ts
@@ -203,7 +203,7 @@ export const tools: Tool[] = [
     ],
     screenshots: [
       'https://framerusercontent.com/images/taEIqG4IODOLsMpmFmsybWAN6G8.png?scale-down-to=1024',
-      'https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg'
+      'https://www.framer.com/creators-assets/_next/image/?url=https%3A%2F%2Fy4pdgnepgswqffpt.public.blob.vercel-storage.com%2Ftemplates%2F49015%2Felements-uxT36v1xzM9B3xlID3iSxVOZYw5H0G&w=3840&q=100'
     ],
     videoUrl: 'https://www.youtube.com/watch?v=example',
     websiteUrl: 'https://framer.com',
